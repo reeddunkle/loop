2c6b5c7a53d4f63e9b578c9ad3437f6ee6f451f5
Reed Dunkle <reeddunkle@gmail.com>
2018-10-24 19:42:31 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConnectedRouter } from 'react-router-redux';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import store, { history } from './store';
+import store from './store';
 import './index.css';
-import App from './App';
+import Root from './Root';
 
-console.log({ ConnectedRouter });
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,9 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import * as storage from 'redux-storage';
 import createEngine from 'redux-storage-engine-localstorage';
-import { Router, Route, browserHistory } from 'react-router';
-import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import reducers from './reducer';
 
-const history = createHistory();
 const rootReducer = storage.reducer(reducers);
 const storageEngine = createEngine('loop');
 
@@ -16,13 +13,10 @@ const storageLoader = storage.createLoader(storageEngine);
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk, storageMiddleware, routerMiddleware(history))
-  )
+  composeWithDevTools(applyMiddleware(thunk, storageMiddleware))
 );
 
 storageLoader(store);
 
 export default store;
-export { history };
 export * from './actions';
